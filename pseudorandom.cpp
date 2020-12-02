419fd13b32c4f1865635536a9fd9b6ac1759155c
pawel1216 <68167420+pawel1216@users.noreply.github.com>
2020-12-02 12:08:04 +0100

    Update pseudorandom.cpp

--- pseudorandom.cpp
+++ pseudorandom.cpp
@@ -1,4 +1,5 @@
 #include <random>
+#include <iostream>
 
 using namespace std;
 
@@ -6,5 +7,6 @@ int main(){
 	std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<int> dis(1, 6);
-	return dis(gen);
+	for(int i=0; i<10; i++)
+		cout<<dis(gen);
 }
